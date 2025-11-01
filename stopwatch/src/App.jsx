ff3b56cc57c5179f521cd6574d53117162911599
archit0111 <sarchit0111@gmail.com>
2025-11-01 23:52:55 +0530

    Stopwatch's first commit

---
+++ stopwatch/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Stopwatch from './Stopwatch.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Stopwatch/>
+    </>
+  )
+}
+
+export default App
---
+++ stopwatch/src/Stopwatch.jsx
@@ -0,0 +1,84 @@
+import { use } from 'react';
+import './Stopwatch.css'
+import { useState, useRef } from 'react'
+
+
+function Stopwatch(){
+    const [time,settime]=useState("00:00:00");
+    const [clk,handleClk]=useState("Start");
+    const c= useRef(false);
+    const intervalRef = useRef(null);
+    const [lap_divs, setDiv]=useState([]);
+    function handleClick(e){
+        if(e.target.name==='start'){
+            if(c.current){
+                    return;
+                }
+            if(clk==="Restart"){
+                toaddDiv();
+            }    
+            c.current=true;
+            intervalRef.current = setInterval(()=>{
+                settime(time=>{
+                    let [total_hour, total_min, total_sec]=time.split(":").map(Number);
+                    total_sec++;
+                    if(total_sec===60){
+                    total_sec=0;
+                    total_min++;
+                    if(total_min===60){
+                        total_min=0;
+                        total_hour++;
+                        if(total_hour===60){
+                            alert("!Day one Completed!");
+                        }
+                    }
+                }
+                total_sec=String(total_sec).padStart(2,"0");
+                total_min=String(total_min).padStart(2,"0");
+                total_hour=String(total_hour).padStart(2,"0");
+            return `${total_hour}:${total_min}:${total_sec}`;
+                });
+            },1000)
+        }else{
+            handleStop();
+            handleClk("Start");
+            settime("00:00:00");
+        }
+    }
+
+
+    function handleStop(){
+        handleClk("Restart");
+        clearInterval(intervalRef.current);
+        intervalRef.current=null;
+        c.current=false;
+    }
+
+    function toaddDiv(){
+
+        setDiv(
+            <div className="lap">
+            <div id="lap_row">LAP {lap_divs.length+1}.</div>
+            <div id="lap_time">{time}</div>
+            </div>
+        )
+    }
+
+
+    return(
+        <>
+        <div id="heading"><h1>STOPWATCH</h1></div>
+        <div className="main_div">
+            <div id="display"><p id="display_para">{time}</p></div>
+            <div className="btn">
+                <div><button id="start_btn" name='start' onClick={handleClick}>{clk}</button></div>
+                <div><button id="stop_btn" name='stop' onClick={handleStop}>Stop</button></div>
+                <div><button id="reset_btn" name='reset' onClick={handleClick}>Reset</button></div>
+            </div>
+        </div>
+        <div className="laps">{lap_divs}</div>
+        </>
+    )
+}
+
+export default Stopwatch
